fb8f7054ef3edb55f3230a822606db7fdbb80018
Dylan Gould <dylangood@gmail.com>
2018-02-04 10:40:47 -0800

    Initial commit - React/Webpack boilerplate

---
+++ client/jsx/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../styles/App.css'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        Hello, World.
+      </div>
+    );
+  }
+}
