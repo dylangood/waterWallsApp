95e0c0f80069f778a4cc3c056e30e09298dc3691
Dylan Gould <dylangood@gmail.com>
2018-02-05 09:21:30 -0800

    Improve error handling in my test case routing.

--- server/routes.js
+++ server/routes.js
@@ -9,20 +9,25 @@ router.get('/', function(req, res) {
   res.send('/client/index.html');
 });
 
-router.get('/api/test1', function(req, res) {
-  var targeted = Services.fillEpisodeWithAds(test.episode, test.campaigns);
+router.get('/api/tests/fillEpisode', function(req, res) {
+  let targeted = Services.fillEpisodeWithAds(test.episode, test.campaigns);
   res.send(targeted);
 });
 
-router.get('/api/test2', function(req, res) {
-  var largestTroughStats = Services.largestTrough(test.checklist[0].input);
-  res.send(largestTroughStats);
+router.get('/api/tests/largestTrough/cases/:caseId', function(req, res) {
+  let checklist = test.checklist[req.params.caseId] ? test.checklist[req.params.caseId].input : undefined;
+  if (checklist) {
+    let largestTroughStats = Services.largestTrough(checklist);
+    res.send(largestTroughStats);
+  } else {
+    res.status(404).send(`Test case #${req.params.caseId} is undefined.`);
+  }
 });
 
 router.post('/api/episodes', function(req, res) {
   const ep = JSON.parse(req.body.episode);
   const campaigns = JSON.parse(req.body.adCampaigns);
-  var finishedEpisode = Services.fillEpisodeWithAds(ep, campaigns);
+  let finishedEpisode = Services.fillEpisodeWithAds(ep, campaigns);
   // Create database entry for /api/episodes/:id/audio
   res.send(finishedEpisode);
 });
